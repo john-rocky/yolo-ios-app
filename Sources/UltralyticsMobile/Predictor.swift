b9aa6c69af94e9a1dbcc12648dd7ba790f66e10f
john-rocky <rockyshikoku@gmail.com>
2025-01-02 14:38:29 +0900

    update swift package

--- Sources/UltralyticsMobile/Predictor.swift
+++ Sources/UltralyticsMobile/Predictor.swift
@@ -2,27 +2,27 @@ import Vision
 import CoreImage
 
 @MainActor
-public protocol ResultsListener {
+protocol ResultsListener {
     func on(predictions: [[String:Any]])
 }
 
 @MainActor
-public protocol InferenceTimeListener {
+protocol InferenceTimeListener {
     func on(inferenceTime: Double)
 }
 
 @MainActor
-public protocol FpsRateListener {
+protocol FpsRateListener {
     func on(fpsRate: Double)
 }
 
-public protocol Predictor{
+protocol Predictor{
     func predict(sampleBuffer: CMSampleBuffer, onResultsListener: ResultsListener?, onInferenceTime: InferenceTimeListener?, onFpsRate: FpsRateListener?)
     func predictOnImage(image: CIImage) -> YOLOResult
     var labels: [String] { get set }
 }
 
-public enum PredictorError: Error{
+enum PredictorError: Error{
     case invalidTask
     case noLabelsFound
     case invalidUrl
