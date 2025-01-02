9b5d49a404829f5a69a6f489d058bedd23525841
john-rocky <rockyshikoku@gmail.com>
2025-01-02 15:18:11 +0900

    update swift package

--- Sources/UltralyticsMobile/ObjectDetector.swift
+++ Sources/UltralyticsMobile/ObjectDetector.swift
@@ -2,7 +2,7 @@ import Foundation
 import Vision
 import UIKit
 
-class ObjectDetector: Predictor {
+class ObjectDetector: Predictor,@unchecked Sendable {
     private var detector: VNCoreMLModel!
     private var visionRequest: VNCoreMLRequest?
     private var currentBuffer: CVPixelBuffer?
