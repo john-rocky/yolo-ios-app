7f6015d193f4df9a0f3c624693119fd24620bf50
john-rocky <rockyshikoku@gmail.com>
2025-01-07 15:36:43 +0900

    update ui elements

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "UltralyticsMobile",
-            resources: [.process("yourModel.mlmodelc")]),
+            resources: [.process("yolo11n.mlmodelc")]),
         .testTarget(
             name: "UltralyticsMobileTests",
             dependencies: ["UltralyticsMobile"]
